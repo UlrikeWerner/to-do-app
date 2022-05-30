b6f0985172f0e64d1070ace52732fb246b31485e
Ulrike Werner <ulrike_werner@web.de>
2022-05-30 17:40:53 +0200

    set init content

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return <HeaderTitle>ToDo List</HeaderTitle>;
+}
+
+const HeaderTitle = styled.h1`
+  font-size: 2rem;
+  text-align: center;
+  color: white;
+  background-color: darkblue;
+  padding: 1rem 0rem;
+`;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,6 @@
+import { nanoid } from "nanoid";
+import styled from "styled-components";
+
+export default function Main() {
+  return <p>main</p>;
+}
