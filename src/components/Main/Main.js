06052fc09ccdb455dd40d80911d0fba74ae3e723
Ulrike Werner <ulrike_werner@web.de>
2022-05-30 18:04:59 +0200

    set localStorage

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,27 @@
+import { React, useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import styled from "styled-components";
 
 export default function Main() {
-  return <p>main</p>;
+  const [toDos, setToDos] = useState(() => {
+    const toDoList = localStorage.getItem("toDo-list");
+    if (toDoList) {
+      return JSON.parse(toDoList);
+    } else {
+      return [
+        {
+          id: nanoid(),
+          toDoText: "Homework: ToDo App",
+          completed: false,
+          archived: false,
+        },
+      ];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("toDo-list", JSON.stringify(toDos));
+  }, [toDos]);
+
+  return <p>form</p>;
 }
