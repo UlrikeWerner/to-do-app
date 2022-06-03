b8985ecb56fec4d6396ab2b73c924d220940df5a
Ulrike Werner <ulrike_werner@web.de>
2022-06-03 15:50:41 +0200

    components need only the id now

--- src/components/Pages/Archive/ArchiveSite.js
+++ src/components/Pages/Archive/ArchiveSite.js
@@ -4,30 +4,18 @@ import Header from "../../Header/Header.js";
 import ToDoContent from "../../ToDos/ToDoContent.js";
 
 export default function ArchiveSite() {
-  const toDoList = useStore((state) => state.toDos);
+  const toDoIds = useStore((state) =>
+    state.toDos.filter((item) => item.archived).map((item) => item.id)
+  );
 
-  function filterArchivedTest() {
-    let returnList;
-    const filterToDo = toDoList
-      .filter((item) => item.archived === true)
-      .map((item) => {
-        return item;
-      });
-
-    if (filterToDo.length > 0) {
-      returnList = filterToDo.map((item) => {
-        return <ToDoContent key={item.id} item={item} />;
-      });
-    } else {
-      console.log("no archive");
-      returnList = <p>You have no archived task!</p>;
-    }
-    return returnList;
-  }
   return (
     <>
       <Header text="ToDo Archive" />
-      {filterArchivedTest()}
+      {toDoIds.length > 0 ? (
+        toDoIds.map((id) => <ToDoContent key={id} id={id} />)
+      ) : (
+        <p>You have no archived task!</p>
+      )}
     </>
   );
 }
