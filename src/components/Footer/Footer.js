5ffb12e25ef61f78525564b529c1231a99010c46
Ulrike Werner <ulrike_werner@web.de>
2022-06-06 20:10:28 +0200

    active navLink change color and navSyle is now a new compontent

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,25 @@
-import styled from "styled-components";
-import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { FooterUl, FooterLi } from "./FooterStyle.js";
 
 export default function Footer() {
+  const location = useLocation();
   return (
     <FooterUl>
-      <FooterLi to="/">Home</FooterLi>
-      <FooterLi to="/archive">Archive</FooterLi>
-      <FooterLi to="/random">Random</FooterLi>
+      <FooterLi to="/" active={(location.pathname === "/").toString()}>
+        Home
+      </FooterLi>
+      <FooterLi
+        to="/archive"
+        active={(location.pathname === "/archive").toString()}
+      >
+        Archive
+      </FooterLi>
+      <FooterLi
+        to="/random"
+        active={(location.pathname === "/random").toString()}
+      >
+        Random
+      </FooterLi>
     </FooterUl>
   );
 }
-
-const FooterUl = styled.section`
-  height: 4rem;
-  position: fixed;
-  bottom: 0;
-  display: flex;
-  justify-content: space-around;
-  background-color: darkblue;
-  width: 95%;
-`;
-
-const FooterLi = styled(NavLink)`
-  padding: 1em 0;
-  list-style: none;
-  text-decoration: none;
-  color: white;
-  &:hover,
-  &:active {
-    transform: scale(1.1);
-    color: lightblue;
-  }
-`;
